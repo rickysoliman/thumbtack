e5a570649523cfa59a0c126d9372e4e857550372
Ricky Marasigan <rickysoliman@gmail.com>
2025-01-31 17:37:46 -0500

    display relative time and create more dummy data

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,14 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../../models/post.model';
-import { NgIf } from '@angular/common';
+import { DatePipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-post',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, DatePipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
 })
 export class PostComponent {
   @Input() post!: Post;
+
+  getRelativeTime(date: Date): string {
+    const now = new Date();
+    const postDate = new Date(date);
+    const diffInSeconds = Math.floor((now.getTime() - postDate.getTime()) / 1000);
+
+    if (diffInSeconds < 60) {
+      return `${diffInSeconds} seconds ago`;
+    } else if (diffInSeconds < 3600) {
+      return `${Math.floor(diffInSeconds / 60)} minutes ago`;
+    } else if (diffInSeconds < 86400) {
+      return `${Math.floor(diffInSeconds / 3600)} hours ago`;
+    } else if (diffInSeconds < 172800) {
+      return `yesterday`;
+    } else {
+      return `${Math.floor(diffInSeconds / 86400)} days ago`;
+    }
+  }
 }
