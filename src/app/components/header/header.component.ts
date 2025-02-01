8de118814f10aa501b17ba706c8d0587fd903917
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-01 12:31:27 -0500

    install angular material and create user-actions component with material icons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserActionsComponent } from '../../user-actions/user-actions.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [UserActionsComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
---
+++ src/app/user-actions/user-actions.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-user-actions',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './user-actions.component.html',
+  styleUrl: './user-actions.component.css'
+})
+export class UserActionsComponent {
+
+}
