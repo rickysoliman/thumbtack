ba32b49744d80fbf8c962e6da513f93e0d7cece4
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-02 11:40:26 -0500

    place posts inside mat-card

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { dummyPosts, Post } from '../../models/post.model';
 import { PostComponent } from '../post/post.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, PostComponent],
+  imports: [CommonModule, PostComponent, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
