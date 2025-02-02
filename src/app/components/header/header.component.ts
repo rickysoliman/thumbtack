79b80b0e97e7720da13909a085d401d9120da717
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-02 17:37:55 -0500

    created search bar, not functioning yet

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { UserActionsComponent } from '../../user-actions/user-actions.component';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AsyncPipe } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [UserActionsComponent],
+  imports: [UserActionsComponent, MatAutocompleteModule, FormsModule, ReactiveFormsModule, AsyncPipe, MatFormFieldModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  searchControl: FormControl = new FormControl();
+  options: string[] = [];
 
+  ngOnInit(): void {
+    this.searchControl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
 }
