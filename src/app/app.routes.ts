d18c5f38208544d92972487eea61ce7946295ce0
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-24 16:15:53 -0500

    remove /home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { HomeComponent } from './components/home/home.component';
 import { BoardComponent } from './components/board/board.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'b/:board', component: BoardComponent },
 ];
