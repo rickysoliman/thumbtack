302c0b78505164547fe49412bb8b23c0d7c33cc7
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-24 16:14:30 -0500

    create board component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { BoardComponent } from './components/board/board.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'b/:board', component: BoardComponent },
 ];
