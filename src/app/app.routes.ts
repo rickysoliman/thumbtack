8e10e1244def85b609b6723056669f3a17ac5cbb
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-25 13:42:08 -0500

    post titles link to comments component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { BoardComponent } from './components/board/board.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'b/:board', component: BoardComponent },
+  { path: 'b/:board/comments/:id', component: CommentsComponent },
 ];
