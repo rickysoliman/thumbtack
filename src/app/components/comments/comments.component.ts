dbd85344933f1a2453947ea5687d1eed3a8423ac
Ricky Marasigan <rickysoliman@gmail.com>
2025-02-27 11:44:21 -0500

    display post in comments page

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,10 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dummyPosts, Post } from '../../models/post.model';
+import { PostComponent } from '../post/post.component';
 
 @Component({
   selector: 'app-comments',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PostComponent],
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
 })
-export class CommentsComponent {}
+export class CommentsComponent {
+  postId: string = '';
+  post: Post | undefined = undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.postId = params['id'];
+      this.post = dummyPosts.find((post) => post.id === this.postId);
+    });
+  }
+}
