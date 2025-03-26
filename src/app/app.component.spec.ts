3ffad9b3691ab4b8012bd577f2fc1e10b21564d9
Ricky Marasigan <rickysoliman@gmail.com>
2025-03-26 19:27:53 -0400

    change all instances of reddit-like-app to thumbtack

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'reddit-like-app' title`, () => {
+  it(`should have the 'thumbtack' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('reddit-like-app');
+    expect(app.title).toEqual('thumbtack');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, reddit-like-app'
+      'Hello, thumbtack'
     );
   });
 });
