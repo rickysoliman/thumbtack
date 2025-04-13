89df37baba5324252195830ba0992fa298c40f90
Ricky Marasigan <rickysoliman@gmail.com>
2025-04-13 13:22:57 -0400

    post displaying in comments page

--- src/app/components/comments-page/comments-page.component.ts
+++ src/app/components/comments-page/comments-page.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Post } from '../../models/post.model';
 import { PostComponent } from '../post/post.component';
 import { CommentComponent } from '../comment/comment.component';
 import { LikesDislikesComponent } from '../likes-dislikes/likes-dislikes.component';
 import { TextBoxComponent } from '../text-box/text-box.component';
+import { map, Observable, switchMap, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Post } from '../../models/post.model';
 
 @Component({
-  selector: 'app-comments',
+  selector: 'app-comments-page',
   standalone: true,
   imports: [
     CommonModule,
@@ -21,20 +23,35 @@ import { TextBoxComponent } from '../text-box/text-box.component';
   styleUrl: './comments-page.component.scss',
 })
 export class CommentsPageComponent {
-  postId: string = '';
+  boardName: string = '';
   post: any | undefined = undefined;
+  comments: any[] = [];
   showCommentForm: boolean = false;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.postId = params['id'];
-      // this.post = dummyPosts.find((post: any) => post.id === this.postId);
-    });
+    this.route.params
+      .pipe(
+        map((params) => params['id']),
+        tap((id) => (this.boardName = this.route.snapshot.params['board'])),
+        switchMap((postId) => this.fetchPost(postId))
+      )
+      .subscribe((resp: any) => {
+        this.post = resp[0].data.children[0].data;
+        this.comments = resp[1].data.children;
+      });
   }
 
-  getRelativeTime(date: Date): string {
+  fetchPost(postId: string): Observable<Post[]> {
+    return this.http
+      .get<any>(
+        `https://www.reddit.com/r/${this.boardName}/comments/${postId}.json`
+      )
+      .pipe(map((resp) => resp));
+  }
+
+  getRelativeTime(date: number): string {
     const now = new Date();
     const postDate = new Date(date);
     const diffInSeconds = Math.floor(
@@ -52,5 +69,11 @@ export class CommentsPageComponent {
     } else {
       return `${Math.floor(diffInSeconds / 86400)} days ago`;
     }
+  }
+
+  decodeHtmlEntities(str: string): string {
+    const txt = document.createElement('textarea');
+    txt.innerHTML = str;
+    return txt.value;
   }
 }
