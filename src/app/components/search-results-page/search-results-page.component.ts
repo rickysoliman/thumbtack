a27f0b6de9545fa3a5f5a39ca8ee1734e2711008
Ricky Marasigan <rickysoliman@gmail.com>
2025-04-27 21:56:25 -0400

    fetching search results from API

--- src/app/components/search-results-page/search-results-page.component.ts
+++ src/app/components/search-results-page/search-results-page.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-search-results-page',
@@ -7,4 +10,26 @@ import { Component } from '@angular/core';
   templateUrl: './search-results-page.component.html',
   styleUrl: './search-results-page.component.scss',
 })
-export class SearchResultsPageComponent {}
+export class SearchResultsPageComponent {
+  searchQuery: string = '';
+  results: any[] = [];
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        map((params) => {
+          return params['q'];
+        }),
+        switchMap((query) => this.fetchSearchResults(query))
+      )
+      .subscribe((resp: any) => {
+        this.results = resp.data.children.map((child: any) => child.data);
+      });
+  }
+
+  fetchSearchResults(query: string): Observable<any> {
+    return this.http.get<any>(`https://www.reddit.com/search.json?q=${query}`);
+  }
+}
